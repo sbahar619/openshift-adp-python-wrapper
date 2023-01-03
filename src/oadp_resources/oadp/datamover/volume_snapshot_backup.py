fccf78f5bb76babcaefde3fafe81002bea879603
Shahaf Bahar <sbahar@redhat.com>
2023-01-03 14:42:46 +0200

    Add vsb methods

--- src/oadp_resources/oadp/datamover/volume_snapshot_backup.py
+++ src/oadp_resources/oadp/datamover/volume_snapshot_backup.py
@@ -1,7 +1,10 @@
 from ocp_resources.resource import NamespacedResource
-from oadp_constants.resources import ApiGroups
+from src.oadp_constants.resources import ApiGroups
 
 
-class VolumeSnapshotLocation(NamespacedResource):
+class VolumeSnapshotBackup(NamespacedResource):
     api_group = ApiGroups.DATAMOVER_OADP_API_GROUP.value
 
+    @classmethod
+    def get_vsbs_by_restore_name(cls, restore_name):
+        return list(cls.get(label_selector=f"velero.io/restore-name={restore_name}"))
